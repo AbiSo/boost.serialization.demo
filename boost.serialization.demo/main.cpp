2fd71c4253690c633d5a3ebef16e05134774756e
Abhijit Sovakar <a.sovakar@gmx.de>
2017-12-09 11:56:55 +0100

    Initial Commit

---
+++ boost.serialization.demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  boost.serialization.demo
+//
+//  Created by Abhijit Sovakar on 09.12.17.
+//  Copyright © 2017 Abhijit Sovakar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
